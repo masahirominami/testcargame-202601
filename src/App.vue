4896ad3148c1ea2537d05f3f9af8ef29fceee14a
Masahiro Minami <masahiro@minamirnd.work>
2026-01-31 07:37:58 +0900

    Initial commit: Scaffold basic Vue.js project structure

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Welcome to the 3D Maze Game</h1>
+    <p>Project is being set up...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
